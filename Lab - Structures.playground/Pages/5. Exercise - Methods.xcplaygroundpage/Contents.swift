f4d92c14ce4a27bc4fd523ce91c0d9ee8e0c921d
Lenayah <lfanesie@gmail.com>
2023-10-02 12:17:34 -0400

    update

--- Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
+++ Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
@@ -10,7 +10,8 @@ struct Book {
     var price: Double
     
 }
-
+let description = Book(title: String, author: <#T##String#>, pages: <#T##Int#>, price: <#T##Double#>)
+print(description)
 
 //:  A `Post` struct has been created for you below, representing a generic social media post. Add a mutating method on `Post` called `like` that will increment `likes` by one. Then create an instance of `Post` and call `like()` on it. Print out the `likes` property before and after calling the method to see whether or not the value was incremented.
 struct Post {
